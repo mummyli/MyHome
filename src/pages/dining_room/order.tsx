2e391b4b41dfb9f69897a529f0be42ea2675759d
shijia <lishijia116804@cdkf.com>
2022-11-07 18:25:25 +0800

    modified:   src/pages/dining_room/order.tsx

--- src/pages/dining_room/order.tsx
+++ src/pages/dining_room/order.tsx
@@ -1,32 +1,58 @@
-import { Component, PropsWithChildren } from 'react'
+import React from 'react'
 import { View } from '@tarojs/components'
-import './order.less'
-import { AtGrid } from "taro-ui"
+import { AtTabs, AtTabsPane, AtDivider } from 'taro-ui'
 
-export default class Order extends Component<PropsWithChildren> {
+export default class Order extends React.Component {
 
-  componentWillMount() { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      current: 0,
+    }
+  }
 
-  componentDidMount() { }
-
-  componentWillUnmount() { }
-
-  componentDidShow() { }
-
-  componentDidHide() { }
+  handleClick(value) {
+    this.setState({
+      current: value
+    })
+  }
 
   render() {
     return (
       <View className='order'>
-        <AtGrid columnNum={2} data={
-          [
-            {
-              image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
-              url: '/pages/dinning_room/order',
-              value: '黎先生的私厨'
-            }
-          ]
-        } />
+        <AtTabs
+          current={this.state.current}
+          scroll
+          height='300px'
+          tabDirection='vertical'
+          tabList={[
+            { title: '家常小炒' },
+            { title: '炖菜' },
+            { title: '烧菜' },
+            { title: '蘸料' },
+            { title: '喝点什么' },
+            { title: '午后甜点' },
+          ]}
+          onClick={this.handleClick.bind(this)}>
+          <AtTabsPane tabDirection='vertical' current={this.state.current} index={0}>
+            <View style='font-size:20px;text-align:center;height:300px;'><AtDivider content='什么也没有' fontColor='#2d8cf0' lineColor='#2d8cf0' /></View>
+          </AtTabsPane>
+          <AtTabsPane tabDirection='vertical' current={this.state.current} index={1}>
+            <View style='font-size:20px;text-align:center;height:300px;'><AtDivider content='什么也没有' fontColor='#2d8cf0' lineColor='#2d8cf0' /></View>
+          </AtTabsPane>
+          <AtTabsPane tabDirection='vertical' current={this.state.current} index={2}>
+            <View style='font-size:20px;text-align:center;height:300px;'><AtDivider content='什么也没有' fontColor='#2d8cf0' lineColor='#2d8cf0' /></View>
+          </AtTabsPane>
+          <AtTabsPane tabDirection='vertical' current={this.state.current} index={3}>
+            <View style='font-size:20px;text-align:center;height:300px;'><AtDivider content='什么也没有' fontColor='#2d8cf0' lineColor='#2d8cf0' /></View>
+          </AtTabsPane>
+          <AtTabsPane tabDirection='vertical' current={this.state.current} index={4}>
+            <View style='font-size:20px;text-align:center;height:300px;'><AtDivider content='什么也没有' fontColor='#2d8cf0' lineColor='#2d8cf0' /></View>
+          </AtTabsPane>
+          <AtTabsPane tabDirection='vertical' current={this.state.current} index={5}>
+            <View style='font-size:20px;text-align:center;height:300px;'><AtDivider content='什么也没有' fontColor='#2d8cf0' lineColor='#2d8cf0' /></View>
+          </AtTabsPane>
+        </AtTabs>
       </View>
     )
   }
