a12fab607fbce1bfd270f9e923ef607b3847b5c5
shijia <lishijia116804@cdkf.com>
2022-11-10 18:26:22 +0800

    modified:   src/pages/dining_room/order.less 	modified:   src/pages/dining_room/order.tsx

--- src/pages/dining_room/order.tsx
+++ src/pages/dining_room/order.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { View, Image } from '@tarojs/components'
-import { AtTabs, AtTabsPane, AtTabBar, AtIcon, AtButton } from 'taro-ui'
+import { AtTabs, AtTabsPane, AtTabBar, AtIcon, AtButton, AtActionSheet, AtActionSheetItem } from 'taro-ui'
 import './order.less'
 
 const getSideBarList = () => {
@@ -56,6 +56,7 @@ export const TabBar: React.FC = () => {
 
   return (
     <AtTabs
+      className='at-tabs-content'
       current={current}
       scroll
       height='400px'
@@ -77,26 +78,59 @@ export const TabBar: React.FC = () => {
 
 export default () => {
 
+  const [cartOpen, setcartOpen] = useState<boolean>(false)
+
   return (
-    <View className='order'>
+    <View className='main-box'>
       <View className='box'>
         <View className='side-bar'>
           <TabBar />
         </View>
       </View>
-      <View className="cart-bar">
-        <AtButton loading type='primary'>购物车</AtButton>
+      <View className='box-bottom'>
+        <View className="cart-bar">
+          <View className='l' onClick={() => { setcartOpen(cartOpen ? false : true) }}>
+            <Image className='img-cart' src='https://s1.ax1x.com/2022/11/10/zpq07d.png' />
+            <View className="badge">7</View>
+          </View>
+          <AtButton className='cart-botton' type='primary'>选好了</AtButton>
+        </View>
+
+        <AtTabBar
+          tabList={[
+            { title: '点餐', image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png' },
+            { title: '已点', image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png' }
+          ]}
+          onClick={(idx) => { return idx }}
+          current={0}
+          fixed={true}
+          className='bottom-menu'
+        />
       </View>
-      <AtTabBar
-        tabList={[
-          { title: '点餐', image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png' },
-          { title: '已点', image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png' },
-          { title: '我的', image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png', text: '100', max: 99 }
-        ]}
-        onClick={(idx) => { return idx }}
-        current={0}
-        fixed={true}
-      />
+
+      <AtActionSheet isOpened={cartOpen} className='cart-item'>
+        <AtActionSheetItem>
+          菜品1
+        </AtActionSheetItem>
+        <AtActionSheetItem>
+          菜品2
+        </AtActionSheetItem>
+        <AtActionSheetItem>
+          菜品3
+        </AtActionSheetItem>
+        <AtActionSheetItem>
+          菜品4
+        </AtActionSheetItem>
+        <AtActionSheetItem>
+          菜品5
+        </AtActionSheetItem>
+        <AtActionSheetItem>
+          菜品6
+        </AtActionSheetItem>
+        <AtActionSheetItem>
+          菜品7
+        </AtActionSheetItem>
+      </AtActionSheet>
     </View>
   )
 }
