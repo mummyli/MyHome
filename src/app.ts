2796af6c5248ba461eae0608510f3427bcc71a0f
shijia <lishijia116804@cdkf.com>
2022-11-22 18:34:47 +0800

    modified:   src/app.ts 	modified:   src/pages/dining_room/order.less

--- src/app.ts
+++ src/app.ts
@@ -5,42 +5,111 @@ import Taro from '@tarojs/taro'
 
 class App extends Component<PropsWithChildren> {
 
-  componentDidMount() { }
+  globalData = {"path": "sxxx"}
+  
+  gotologin() {
+    var that = this
+
+    //清除缓存
+    Taro.clearStorageSync();
+    // 登录
+    Taro.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          console.log(res.code);
+          //发起网络请求
+          Taro.request({
+            url: that.globalData.path + '/miniLogin/login.ajax',
+            data: {
+              code: res.code
+            },
+            success: res => {
+              let mylogininfo = res.data.data
+              if (mylogininfo != null) {
+                Taro.setStorageSync('logininfo', mylogininfo)
+                console.log(mylogininfo)
+                //登录验证  拿openid换AUTH_TICKET
+                Taro.request({
+                  // method: 'post',
+                  url:
+                    that.globalData.path +
+                    '/miniLogin/loginCheck.ajax',
+                  header: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  },
+                  data: {
+                    openid: mylogininfo.openid,
+                    unionid: mylogininfo.unionid ? mylogininfo.unionid : '',
+                    phoneNo: ''
+                  },
+                  success: res => {
+                    let mdata = res.data
+                    if (mdata.code == 200) {
+                      Taro.showToast({
+                        title: '登录成功',
+                        icon: 'success',
+                        duration: 2000
+                      })
+                      // 存储AUTH_TICKET
+                      Taro.setStorageSync(
+                        'AUTH_TICKET',
+                        mdata.data.AUTH_TICKET
+                      )
+                      // 携带AUTH_TICKET请求接口，获取数据
+                      Taro.request({
+                        // method: 'post',
+                        url:
+                          that.globalData.path + '/comLogin/statffCompany.ajax',
+                        header: {
+                          'Content-Type': 'application/x-www-form-urlencoded',
+                          'weixinauth': mdata.data.AUTH_TICKET
+                        },
+                        success: function (res) {
+                          console.log(res)
+                        }
+                      })
+
+                    } else if (mdata.code == 400) {
+                      //此ID未绑定
+                      //获取手机号码绑定
+                    }
+                  }
+                })
+              } else {
+                console.log(res.data)
+              }
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  }
+  
+  componentDidMount() {
+    Taro.checkSession({
+      success: () => {
+        this.gotologin();
+        //session_key 未过期，并且在本生命周期一直有效
+      },
+      fail: () => {
+        // session_key 已经失效，需要重新执行登录流程
+        // 登录
+        this.gotologin();
+      }
+    });
+  }
+  
 
   componentDidShow() { }
 
   componentDidHide() { }
 
-  gotoLogin = () => {
-
-    Taro.clearStorageSync();
-
-    Taro.login({
-      success: res => {
-        console.log(res.code);
-      }
-    })
-  }
 
   render() {
 
-    Taro.checkSession({
-      success: function () {
-        console.log("session_key 未过期")
-        //session_key 未过期，并且在本生命周期一直有效
-      },
-      fail: function () {
-        // session_key 已经失效，需要重新执行登录流程
-        // 登录
-        Taro.clearStorageSync();
-
-        Taro.login({
-          success: res => {
-            console.log(res.code);
-          }
-        })
-      }
-    })
     // this.props.children 是将要会渲染的页面
     return this.props.children
   }
