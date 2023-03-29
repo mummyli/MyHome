24a6ed49e63b8930bead1d3c859d5e03a4395887
shijia <lishijia116804@cdkf.com>
2023-03-29 18:54:12 +0800

    add handle login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,110 +3,78 @@ import { Component, useState } from 'react'
 import { View, Button, Image, Text, Input } from '@tarojs/components'
 import './login.less'
 
-
 type User = {
   nickName: string;
   avatarUrl: string;
 }
 
-type State = {
-  isAuthorized: boolean;
-  userInfo: User | null;
+const handleLogin = (setIsAuthorized) => {
+  try {
+    Taro.login({
+      success: res => {
+        // 发送 res.code 到后台换 查看是否已经注册
+        if (res.code) {
+          Taro.request({
+            url: "/login",
+            data: {
+              code: res.code
+            },
+            success: res => {
+              let logininfo = res.data.data
+
+              if (logininfo != null) {
+                if (logininfo.code == 200) {
+                  if (logininfo.detail == "not register") {
+                    setIsAuthorized(false)
+                  } else {
+                    Taro.setStorageSync('AUTH_TICKET', logininfo.AUTH_TICKET)
+                    Taro.setStorageSync('nickname', logininfo.nickname)
+                    Taro.navigateTo({ url: "/pages/index/index" })
+                  }
+                } else {
+                  console.log(logininfo)
+                }
+              } else {
+                console.log(res.data)
+              }
+            }
+          }
+          )
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-// const [nickname, setNickName] = useState<string>('微信用户')
-// const [avatar, setAvatar] = useState<string>('None')
+export default () => {
 
-export default class Login extends Component<{}, State> {
+  const [avatar, setAvatar] = useState<string>()
+  const [nickName, setNickName] = useState<string>()
+  const [isAuthorized, setIsAuthorized] = useState<boolean>()
+  const [userInfo, setUserInfo] = useState<User>()
 
-  config: Config = {
-    navigationBarTitleText: '登录授权'
-  }
-
-  state: State = {
-    isAuthorized: false,
-    userInfo: null,
-  }
-  nickname: string
-  avatar: string
-
-  async componentDidMount() {
-    const isAuthorized = await this.checkAuthorized()
-    this.setState({
-      isAuthorized,
-    })
-  }
-
-  async checkAuthorized(): Promise<boolean> {
-    try {
-      await Taro.getSetting()
-      const res = await Taro.getUserInfo()
-      console.log(res.userInfo)
-      if (res.errMsg === 'getUserInfo:ok') {
-        this.setState({
-          userInfo: res.userInfo
-        })
-        return true
-      }
-    } catch (error) {
-      console.error(error)
-    }
-    return false
-  }
-
-  async handleLogin() {
-    try {
-      const res = await Taro.login()
-      const { code } = res
-      const userInfo = await this.getUserInfo()
-      this.setState({
-        isAuthorized: true,
-        userInfo
-      })
-      // TODO: 发送用户信息到后端
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  async getUserInfo(): Promise<User> {
-    try {
-      const res = await Taro.getUserInfo()
-      if (res.errMsg === 'getUserInfo:ok') {
-        return res.userInfo
-      }
-    } catch (error) {
-      console.error(error)
-    }
-    return { nickName: '', avatarUrl: '' }
-  }
-
-
-
-  render() {
-    const { isAuthorized, userInfo } = this.state
-
-    return (
-      <View className='index'>
-        {isAuthorized && userInfo ? (
-          <View>
-            <Button open-type="chooseAvatar"
-                    onChooseAvatar={(e) => {
-                      this.avatar = e.detail.avatarUrl
-                    }}  // 在taro中使用的是onChooseAvatar
-                    className="info-content__btn">
-              <Image className="info-content__avatar" src={this.avatar}/> 
-            </Button>
-            <Input type="nickname" 
-	                 className="info-content__input"
-	                 placeholder="请输入昵称"
-	                 value={this.nickname}
-	                 onInput={(e) => this.nickname = e.detail.value}/>
-          </View>
-        ) : (
-          <Button className='login-btn' onClick={this.handleLogin.bind(this)}>登录</Button>
-        )}
-      </View>
-    )
-  }
+  return (
+    <View className='index'>
+      {isAuthorized ? (
+        <View>
+          <Button open-type="chooseAvatar"
+            onChooseAvatar={(e) => setAvatar(e.detail.avatarUrl)}  // 在taro中使用的是onChooseAvatar
+            className="info-content__btn">
+            <Image src={avatar} className="avatar" />
+          </Button>
+          <Input type="nickname"
+            className="info-content__input"
+            placeholder="请输入昵称"
+            value={nickName}
+            onInput={(e) => setNickName(e.detail.value)} />
+        </View>
+      ) : (
+        <Button className='login-btn' onClick={() => handleLogin(setIsAuthorized)}>登录</Button>
+      )}
+    </View>
+  )
 }
